b1d9d9dc9d9c6772e205598f4fefceb469d5beae
ixxveon <igayeon322s@gmail.com>
2026-01-23 18:21:40 +0900

    백업

--- 2. Database/General/Scripts/Script-1.sql
+++ 2. Database/General/Scripts/Script-1.sql
@@ -1,117 +1,56 @@
--- employees라는 데이터베이스 선택
-use employees;
-
-select emp_no, birth_date from employees;
-select hire_date from employees;
-select emp_no, from_date, to_date from dept_manager;
-select * from departments;
-
-insert into employees
-values (1,
-        '2000-01-01',
-        'jaeseop',
-        'kim',
-        'M',
-        now());
-
-select * from employees;
-
-INSERT INTO departments
-values('d010',
-       'Korea IT Department');
-
-INSERT INTO employees 
-VALUES (2,
-        '2002-01-01',
-        'gayeon',
-        'lee',
-        'F',
-        now());
-
-INSERT INTO dept_emp 
-VALUES (1,
-        'd010',
-        now(),
-        '9999-01-01');
-
-SELECT * FROM departments;
-SELECT * FROM employees;
-SELECT * FROM dept_emp;
-
-UPDATE employees
-SET first_name = '길동'
-WHERE emp_no = 10001;
-
-UPDATE employees
-SET last_name = '홍',
-    hire_date = now()
-WHERE emp_no = 10001;
-
-SELECT * FROM employees;
-
-UPDATE departments
-SET dept_name = 'Tech Sales'
-WHERE dept_no = 'd007';
-
-UPDATE employees
-SET birth_date = '1977-07-07'
-WHERE last_name = 'Erie';
-
-SELECT * FROM departments;
-
-SELECT * FROM employees WHERE last_name='Erie';
-
-DELETE FROM employees
-WHERE emp_no = 10005;
-
-DELETE FROM employees 
-WHERE hire_date = '1993-05-12';
-
-SELECT * FROM employees WHERE hire_date = '1993-05-12';
-
-DELETE FROM employees 
-WHERE emp_no = 28847;
-
-SELECT * FROM employees
-WHERE emp_no = 28847;
-
-CREATE TABLE test_member (
-	member_id varchar(50) COMMENT '아이디',
--- 	비밀번호는 저장할 때 암호화해서 저장하기 때문에 255
-	member_pwd varchar(255) COMMENT '비밀번호',
-	member_name varchar(50) COMMENT '이름',
-	member_age TINYINT COMMENT '나이',
-	member_in_date datetime COMMENT '가입일'
+CREATE TABLE MEMBER_notnull (
+	id varchar(50) NOT NULL,
+	pwd varchar(250) NOT NULL,
+	name varchar(50),
+	age int,
+	in_date datetime
 );
 
-DROP TABLE test_member;
+INSERT INTO member_notnull
+VALUES ('test', 'qwer1234!', null, null, now());
 
-CREATE TABLE free_board (
-	board_no int COMMENT '게시판 번호',
-	board_titile varchar(100) COMMENT '게시판 제목',
-	board_content text COMMENT '게시판 내용',
-	board_date datetime COMMENT '게시판 작성일',
-	board_views int COMMENT '게시판 조회수'
+INSERT INTO member_notnull (id, pwd, in_date)
+VALUES ('test2', 'qwer1234!', now());
+
+SELECT * FROM member_notnull;
+
+######################################################################
+CREATE TABLE member_unique (
+	id varchar(50) NOT NULL UNIQUE,
+	pwd varchar(250) NOT NULL 
 );
 
-INSERT INTO free_board
-VALUES (1,
-		'개인 게시판',
-		'Hello, World!',
-		now(),
-		10
+INSERT INTO member_unique values('test', 'qwer1234!');
+
+CREATE TABLE member_check (
+	gender varchar(1) CHECK (gender IN('W', 'M'))
 );
 
-INSERT INTO free_board
-VALUES (2,
-		'모두의 게시판',
-		'Hello, Database!',
-		now(),
-		3
+INSERT INTO member_check VALUES('W');
+INSERT INTO member_check VALUES('w');
+INSERT INTO member_check VALUES('asdasd');
+SELECT * FROM member_check;
+
+######################################################################
+CREATE TABLE member_default (
+	id varchar(50),
+	in_date datetime DEFAULT now()
 );
 
-SELECT * FROM free_board;
+INSERT INTO member_default(id) values('test');
+SELECT * FROM member_default;
 
+######################################################################
+CREATE TABLE member_pk (
+	member_pk_id int PRIMARY KEY auto_increment,
+	id varchar(50)
+);
+
+DROP TABLE member_pk;
+
+INSERT INTO member_pk(id) VALUES ('test');
+INSERT INTO member_pk VALUES (NULL, 'test');
+SELECT * FROM member_pk;
 
 
 
---
+++ 2. Database/General/Scripts/Script-8.sql
@@ -0,0 +1,81 @@
+-- 입사일이 1993년 2월 19일 이면서 생일이 1964년 10월 24일 직원의 이름을 구하고
+-- 다시 해당 이름으로 조건을 검색해서 사번(emp_no)을 구해야 하는 경우
+
+
+SELECT first_name, last_name FROM employees
+WHERE hire_date = '1993-02-19'
+AND birth_date = '1964-10-24';
+
+SELECT emp_no FROM employees
+WHERE first_name = 'Conrado'
+AND last_name = 'Serra';
+
+-- 서브쿼리 사용
+SELECT emp_no FROM employees
+WHERE first_name = (SELECT first_name FROM employees
+					WHERE hire_date = '1993-02-19'
+					AND birth_date = '1964-10-24')
+AND last_name = (SELECT last_name FROM employees
+				 WHERE hire_date = '1993-02-19'
+				 AND birth_date = '1964-10-24');
+
+SELECT emp_no
+FROM employees
+WHERE (first_name, last_name) = (SELECT first_name, last_name
+								 FROM employees 
+								 WHERE hire_date = '1993-02-19'
+								 AND birth_date = '1964-10-24');	
+
+-- 직원 중에서 emp_no가 가장 높은 직원 찾기
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (SELECT max(emp_no) 
+				FROM employees);
+
+-- limit: 조회된 데이터 중 상단 하나만 (order by 필수)
+SELECT max(emp_no), first_name, last_name, emp_no
+FROM employees
+GROUP BY first_name, last_name, emp_no
+ORDER BY emp_no DESC
+LIMIT 1;
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY emp_no DESC
+LIMIT 1;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date = (SELECT min(hire_date)
+				   FROM employees);
+
+-- 전체 평균보다 높은 연봉을 받는 이름 조회
+-- 조인 없이
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (SELECT emp_no FROM salaries 
+				WHERE salary > (SELECT avg(salary) FROM salaries)
+				ORDER BY salary DESC
+				LIMIT 1);
+
+-- 평균 사번보다 높은 직원의 이름만 출력
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no >= (SELECT avg(emp_no)
+				 FROM employees);
+
+-- SELECT *
+-- FROM employees e
+-- WHERE hire_date > (SELECT avg(hire_date)
+-- 				   FROM employees
+-- 				   WHERE emp_no = e.emp_no);
+
+SELECT emp_no, salary
+FROM salaries
+WHERE salary IN (SELECT salary
+				 FROM salaries s
+				 ORDER BY salary DESC);
+
+
+
+
