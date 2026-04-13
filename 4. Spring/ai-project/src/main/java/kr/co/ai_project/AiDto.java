d333a90349603861844b2f42b0fe41f586dd19c9
ixxveon <igayeon322s@gmail.com>
2026-04-13 18:13:52 +0900

    2026-04-13

---
+++ 4. Spring/ai-project/src/main/java/kr/co/ai_project/AiDto.java
@@ -0,0 +1,14 @@
+package kr.co.ai_project;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AiDto {
+
+	@Setter
+	@Getter
+	public static class RequestAi {
+		private String prompt;
+	}
+}
+  
